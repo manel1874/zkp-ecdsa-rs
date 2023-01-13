8bc0eee55f118edfc3f262b5757eede55dcb1eb7
manel1874 <manel1874@gmail.com>
2023-01-13 00:04:13 +0000

    Working on commitment module. TODO: understand better the concept of borrowing.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+/* use std::error::Error;
 use std::vec::Vec;
 use num_bigint::BigUint;
 use secp256k1::{Secp256k1, PublicKey};
@@ -51,12 +51,121 @@ pub async fn prove_signature_list(
         pk_x,
         pk_y,
         sig_proof,
-        membership_proof
+        members hip_proof
     })
 }
+*/
+
+//use secp256k1;
+
+/*
+async fn prove_point_add(
+    params: PedersenParams,
+    P: GroupPoint,
+    Q: GroupPoint,
+    R: GroupPoint,
+    PX: Commitment,
+    PY: Commitment,
+    QX: Commitment,
+    QY: Commitment,
+    RX: Commitment,
+    RY: Commitment
+) -> Result<PointAddProof, Box<dyn Error>> {
+    if !P.add(Q).eq(R) {
+        return Err(From::from("Points don't add up!"));
+    }
+    let prime = params.c.order;
+    let C1 = PX;
+    let C2 = QX;
+    let C3 = RX;
+    let C4 = PY;
+    let C5 = QY;
+    let C6 = RY;
+    let coord_p = P.to_affine();
+    let coord_q = Q.to_affine();
+    let coord_r = R.to_affine();
+    if coord_p.is_none() {
+        return Err(From::from("P is at infinity"));
+    }
+    if coord_q.is_none() {
+        return Err(From::from("Q is at infinity"));
+    }
+    if coord_r.is_none() {
+        return Err(From::from("R is at infinity"));
+    }
+    let coord_p = coord_p.unwrap();
+    let coord_q = coord_q.unwrap();
+    let coord_r = coord_r.unwrap();
+    let x1 = coord_p.x;
+    let y1 = coord_p.y;
+    let x2 = coord_q.x;
+    let y2 = coord_q.y;
+    let x3 = coord_r.x;
+    let i7 = pos_mod(x2 - x1, prime);
+    let i8 = inv_mod(i7, prime);
+    let i9 = pos_mod(y2 - y1, prime);
+    let i10 = pos_mod(i8 * i9, prime);
+    let i11 = pos_mod(i10 * i10, prime);
+    let i12 = pos_mod(x1 - x3, prime);
+    let i13 = pos_mod(i10 * i12, prime);
+    let C7 = C2.sub(C1);
+    let C8 = params.commit(i8);
+    let C9 = C5.sub(C4);
+    let C10 = params.commit(i10);
+    let C11 = params.commit(i11);
+    let C12 = C1.sub(C3);
+    let C13 = params.commit(i13);
+    let C14 = Commitment::new(params.g, params.c.new_scalar(BigInt::from(0)));
+    let pi8 = await prove_mult(params, i7, i8, BigInt::from(1), C7, C8, C14)?;
+    let pi10 = await prove_mult(params, i8, i9, i10, C8, C9, C10)?;
+    let pi11 = await prove_mult(params, i10, i10, i11, C10, C10, C11)?;
+
+    let Cint = C3.p + C1.p + C2.p;
+    let pix = await prove_equality(params, i11, C11, Cint)?;
+    let pi12 = await prove_mult(params, i10, i12, i13, C10, C12, C13)?;
+
+    let Cint = C6.p + C4.p;
+    let piy = await prove_equality(params, i13, C13, Cint)?;
+    
+    Ok(PointAddProof{C8,C9,C10,C11,C12,C13,pi8,pi10,pi11,pix,pi12,piy})
+}
+
+*/
+
+
+// use std::error::Error;
+use openssl::ec::{EcGroup, EcPoint};
+pub mod pedersen;
+
+use openssl::nid::Nid;
+use openssl::bn::{BigNum};
 
 
 
 fn main() {
     println!("Hello, world!");
+
+     
+    // Create a new P256 curve object
+    let group = EcGroup::from_curve_name(Nid::X9_62_PRIME256V1).unwrap();
+
+    // Generate two poins randomly
+    //let P1 = EcKey::generate(&group)?;
+    //let p2 = EcKey::generate(&group).unwrap();
+    //let p = group.generator();
+    let point = EcPoint::new(&group).unwrap();
+
+    let n43 = "43";
+    let n2 = "2";
+    let mut res = BigNum::new().unwrap();
+    let bign43 = BigNum::from_dec_str(n43).unwrap();
+    let bign2 = BigNum::from_dec_str(n2).unwrap();
+    
+    res.checked_add(&bign43,&bign2).unwrap();
+
+    println!("{}", res);
+
+    let com1 = pedersen::Commitment::new(group, point, bign43);
+    let com2 = pedersen::Commitment::new(group, point, bign43);
+
 }
---
+++ src/pedersen.rs
@@ -0,0 +1,101 @@
+use openssl::ec::{EcGroup, EcPoint};
+use openssl::bn::{BigNum, BigNumContext};
+//use std::convert::TryFrom;
+
+pub struct Commitment {
+    g: EcGroup,
+    p: EcPoint,
+    r: BigNum,
+}
+
+
+impl Commitment {
+
+    pub fn new(g: EcGroup, p: EcPoint, r: BigNum) -> Self {
+        Commitment{ g, p, r }
+    }
+
+    pub fn add(&self, c: &Commitment) -> Commitment {
+        // Set curve | TODO: improve this approach
+        let group_name = self.g.curve_name().unwrap();
+        let group = EcGroup::from_curve_name(group_name).unwrap();
+        // Compute sum of points
+        let mut ctx = BigNumContext::new().unwrap();
+        let mut sum_p = EcPoint::new(&self.g).unwrap();
+        sum_p.add(&self.g, &self.p, &c.p, &mut ctx).unwrap();
+        // Compute sum of random values
+        let mut sum_r = BigNum::new().unwrap();
+        sum_r.checked_add(&self.r, &c.r).unwrap();
+
+        Commitment::new(group, sum_p, sum_r)
+    }
+
+
+    //fn mul()
+
+    //fn sub()
+
+}
+
+/* 
+impl Commitment {
+    fn new(p: EcPoint, r: BigInt) -> Self {
+        Commitment { p, r }
+    }
+    fn add(&self, c: &Commitment) -> Commitment {
+        let p = self.p.add(&c.p).unwrap();
+        let r = self.r.clone() + &c.r;
+        Commitment::new(p, r)
+    }
+    fn mul(&self, k: &BigInt) -> Commitment {
+        let sk = BigUint::from_bigint(k);
+        let sk = match sk {
+            Some(sk) => sk,
+            None => return Commitment::new(EcPoint::new(self.p.group()).unwrap(), BigInt::zero()),
+        };
+        let sk = EcScalar::from(sk);
+        let p = self.p.mul(&sk).unwrap();
+        let r = &self.r * k;
+        Commitment::new(p, r)
+    }
+    fn sub(&self, c: &Commitment) -> Commitment {
+        let p = self.p.sub(&c.p).unwrap();
+        let r = self.r.clone() - &c.r;
+        Commitment::new(p, r)
+    }
+}
+
+struct PedersenParams {
+    c: EcGroup,
+    g: EcPoint,
+    h: EcPoint,
+}
+
+impl PedersenParams {
+    fn new(c: EcGroup, g: EcPoint, h: EcPoint) -> Self {
+        PedersenParams { c, g, h }
+    }
+    fn eq(&self, o: &PedersenParams) -> bool {
+        self.c == o.c && self.g == o.g && self.h == o.h
+    }
+    fn commit(&self, input: &BigInt) -> Commitment {
+        let r = BigInt::rand(256);
+        let v = BigUint::from_bigint(input);
+        let v = match v {
+            Some(v) => v,
+            None => return Commitment::new(EcPoint::new(self.c).unwrap(), BigInt::zero()),
+        };
+        let v = EcScalar::from(v);
+        let p = self.h.mul_with_scalars(&[&r, &v]).unwrap();
+        Commitment::new(p, r)
+    }
+}
+
+fn generate_pedersen_params(c: &EcGroup, g: Option<&EcPoint>) -> PedersenParams {
+    let g = g.unwrap_or_else(|| c.generator().unwrap());
+    let r = BigInt::rand(256);
+    let h = g.mul(&r).unwrap();
+    PedersenParams::new(c.clone(), g.clone(), h)
+}
+
+*/
