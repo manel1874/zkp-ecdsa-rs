3bcd65866fee236ab012e049eb4989f0f8274c1e
manel1874 <manel1874@gmail.com>
2023-02-13 23:49:33 +0000

    Working on multimult.rs module

--- src/curves/multimult.rs
+++ src/curves/multimult.rs
@@ -1,3 +1,48 @@
+use std::cmp::Ordering;
+
+use openssl::ec::{EcGroup, EcGroupRef, EcPoint, EcPointRef, PointConversionForm};
+use openssl::bn::BigNum;
+
+pub struct Pair {
+    pub pt: EcPoint,
+    pub scalar: BigNum
+}
+
+impl Pair {
+    pub fn new(pt: EcPoint, scalar: BigNum) -> Self {
+        Pair { pt, scalar }
+    }
+
+    pub fn cmp(&self, b: &Pair) -> Ordering {
+        self.scalar.cmp(&b.scalar)
+    }
+}
+
+pub struct Ptidx {
+    pub pt: EcPoint,
+    pub idx: usize
+}
+
+pub struct MultiMult<'a> {
+    group: &'a EcGroupRef,
+    pairs: Vec<Pair>,
+    known: Vec<Ptidx>,
+}
+
+
+
+impl<'a> MultiMult<'a> {
+    fn new(g: &'a EcGroupRef) -> Self {
+        MultiMult {
+            group: g,
+            pairs: vec![],
+            known: vec![],
+        }
+    }
+}
+
+
+
 /*
 pub struct Pair {
     pub pt: Point,
